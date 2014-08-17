57a81d0d564504dc8f8d98d8d2ae111e2951a937
LennyPenny <l.bernhardt@live.de>
2014-08-17 14:56:53 +0200

    init

---
+++ lpAuto.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name          lpAuto
+// @author        Lenny. (@ohitsLenny)
+// @namespace     lpAuto
+// @description   This will auto update threads using the LabPunch api
+// @include       *facepunch.com/showthread*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
+// @resource      
+// @version       1.0
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+$(document).ready(function()
+{
+	alert("jQuery is loaded");
+});
